88a50014c7e6fdb92e8c0302e6843ec33031af4e
mksalin <mksalin@users.noreply.github.com>
2018-09-18 10:37:27 +0300

    added dummy login page
    dummy login page

---
+++ Ionic_test_app/myIonicTestApp/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+ production: false,
+ firebase: {
+    apiKey: "AIzaSyBIlv4tDSGVk4CAMW__9fQeY_UIrdjp_fs",
+    authDomain: "testproject-c9241.firebaseapp.com",
+    databaseURL: "https://testproject-c9241.firebaseio.com",
+    projectId: "testproject-c9241",
+    storageBucket: "testproject-c9241.appspot.com",
+    messagingSenderId: "863924059921"
+ }
+};
